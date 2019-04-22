59b36c5dfd865a7f7d426e9eefd0ae9d3a1fa761
Unknown <jromio4968@sdsu.edu>
2019-04-21 23:19:09 -0700

    Added START/END Directives
    >Swapped .sic/.lis printing to be correct
    
    >Fixed buffer overflow from immediate values by deleting a space in sprintf operand
    
    >Added START and END directives
    	-END directive displays LOCCTR when it should 
              show nothing, still need to fix this

--- main.c
+++ main.c
@@ -69,6 +69,7 @@ int main(int argc, const char * argv[])
     int LOCCTR = 0;
     int nextaddr = 0; /* address of next instruction */
 	link head = NULL;
+	head = InsertSTARTDirective(head, H->name, H->startadr);
 	for(textnx=0; textnx<GetTcount(); textnx++)
 	{
         LOCCTR = T[textnx]->startadr;
@@ -80,6 +81,7 @@ int main(int argc, const char * argv[])
         LOCCTR += T[textnx]->reclength;
         head = InsertRESDirectives(head, LOCCTR, nextaddr);
 	}
+	head = InsertENDDirective(head, H->startadr);
 	/******** END OF STEP 2 *******/
     
     //test//
@@ -106,9 +108,9 @@ int main(int argc, const char * argv[])
            E->firstinst);
 	printf("\nSOURCE CODE DISASSEMBLEMENT PROGRESS\n");
 	PrintList(head);
-	PrintSIC(head, sicfname);
-	PrintLisFile(head, lisfname);
     ///////
+	WriteFile(head, sicfname, 0);
+	WriteFile(head, lisfname, 1);
 	FreeMem(objfname, symfname, H, T, M, E, head);
     return 0;
 }
